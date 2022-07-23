8c8b3183c8fa59699e381a8e1defd70326f86f11
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2022-07-23 15:19:17 -0400

    Validando a data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns'; // parseiso converte string no obj New Date do js
 
 const appointmentsRouter = Router();
 
-const listAppointments = [];
+interface Appointment {
+  id: string;
+  providerName: string;
+  date: Date;
+}
+
+const listAppointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { providerName, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = listAppointments.find(item =>
+    isEqual(parsedDate, item.date),
+  );
+
+  if (findAppointmentInSameDate)
+    return response
+      .status(400)
+      .json({ message: 'Já existe um agendamento marcado nesta data.' });
+
   const appointment = {
     id: uuid(),
     providerName,
-    date,
+    date: parsedDate,
   };
 
   listAppointments.push(appointment);
