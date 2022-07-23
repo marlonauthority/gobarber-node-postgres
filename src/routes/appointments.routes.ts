ccd63c2a8b0121b0a8ca86f43da286be5cb2d2ea
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2022-07-23 13:41:08 -0400

    configurando primeira rota para agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
+const listAppointments = [];
+
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'Agendamento criado' });
+  const { providerName, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    providerName,
+    date,
+  };
+
+  listAppointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
